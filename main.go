35cfe788f92a0205159dbc87c5ec3ecd6dbca433
Daniel Kofler <fox27374@gmail.com>
2025-08-04 08:50:54 +0200

    trapReceiver is called from main function, channels are monitored

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
-	"sync"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -14,16 +15,17 @@ const (
 )
 
 var (
-	config       Config
-	logger       *slog.Logger
-	pollEnabled  = false
-	pollInterval = 60
-	trapEnabled  = false
-	trapPort     = 8162
+	debug          = true
+	config         Config
+	logger         *slog.Logger
+	pollEnabled    = false
+	pollInterval   = 60
+	trapEnabled    = false
+	trapListenAddr = "0.0.0.0"
+	trapPort       = 8162
 )
 
 func init() {
-	debug := false
 	var logLevel slog.Level
 	if debug {
 		logLevel = slog.LevelDebug
@@ -31,14 +33,14 @@ func init() {
 		logLevel = slog.LevelInfo
 	}
 
-	// Create and configure the handler.
+	// Create and configure log handler
 	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: logLevel,
 	})
 
-	// Set the global logger variable.
+	// Set the global logger variable
 	logger = slog.New(handler)
-	// You can also set it as the default logger for the entire application.
+	// Set as default logger
 	slog.SetDefault(logger)
 }
 
@@ -50,11 +52,19 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Create channels for trap communication
+	trapDataChan := make(chan SNMPData)
+	trapErrChan := make(chan error)
+
+	// Create a channel to listen for OS signals for graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
 	// Start trap receiver
 	logger.Info("Trap receiver", slog.Any("enabled", trapEnabled))
 	if trapEnabled {
 		logger.Info(fmt.Sprintf("Trap receiver enabled, starting on port %d", trapPort))
-		go trapReceiver(config.Trap)
+		go trapReceiver(config.Trap, trapDataChan, trapErrChan)
 	}
 
 	// Start polling devices
@@ -62,60 +72,40 @@ func main() {
 	if pollEnabled {
 		logger.Info(fmt.Sprintf("Device polling enabled, starting with an interval of %d seconds", pollInterval))
 		ticker := time.NewTicker(time.Duration(pollInterval) * time.Second)
-		defer ticker.Stop()
-		for {
-			pollAndSend()
-			<-ticker.C
-		}
-	}
-}
 
-// func trapAndSend() {
-// 	dataChan := make(chan SNMPData)
-// 	errChan := make(chan error)
-// }
-
-func pollAndSend() {
-	dataChan := make(chan SNMPData)
-	errChan := make(chan error)
-	var wg sync.WaitGroup
-	var pollResults []SNMPData
-
-	// Launch goroutines for polling
-	for _, device := range config.Devices {
-		wg.Add(1)
-		go func(d DeviceConfig) {
-			defer wg.Done()
-			pollDevice(d, dataChan, errChan)
-		}(device)
-	}
-
-	// Close channels once all workers are done
-	go func() {
-		wg.Wait() // Wait for all pollDevice goroutines to complete
-		close(dataChan)
-		close(errChan)
-	}()
-
-	// Read from errChan
-	go func() {
-		for err := range errChan {
-			if err != nil {
-				logger.Error("Error received from go routine", slog.Any("error", err))
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					err := pollAndSend()
+					if err != nil {
+						logger.Error("device polling failed", slog.Any("error", err))
+					}
+				case <-quit:
+					ticker.Stop()
+					return
+				}
 			}
-		}
-	}()
-
-	// Read from dataChan and fill the pollResults slice
-	for res := range dataChan {
-		if res != nil {
-			pollResults = append(pollResults, res)
-		}
+		}()
 	}
 
-	// Send data to Splunk
-	err := sendToSplunkHec(pollResults)
-	if err != nil {
-		fmt.Println(err)
+	slog.Info("Application started, waiting for traps or polling events")
+
+	// Use a for-select loop to continuously read from trap channels
+	for {
+		select {
+		case data := <-trapDataChan:
+			slog.Info("New trap data received", slog.Any("data", data))
+			s := []SNMPData{data}
+			err := sendToSplunkHec(s)
+			if err != nil {
+				slog.Error(fmt.Sprintf("sending data to Splunk failed: %v", err))
+			}
+		case err := <-trapErrChan:
+			slog.Error("Trap receiver error", slog.Any("error", err))
+		case <-quit:
+			slog.Info("Received shutdown signal, stopping...")
+			return
+		}
 	}
 }
